97c9aa4fa97d76c3df2ecc0ac452377f5c291717
isamartins-engcomput <isadoramartins1906@gmail.com>
2025-04-07 00:31:09 -0300

    Add files via upload

---
+++ lista-funcoes/1.c
@@ -0,0 +1,15 @@
+// 1 - Faça uma função que não receba parâmetros e escreva na tela (print) o texto “OI”.
+
+#include <stdio.h>
+
+void printar ()
+{
+  printf ("\nOI\n\n");
+}
+
+int main ()
+{ 
+  printar(); 
+  
+return 0;
+}
